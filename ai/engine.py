ffb1f7e8db2b7342f24e686a68b5c5d3ad818e96
Chris kaltsas <140308973+CodebindPyth@users.noreply.github.com>
2026-09-03 19:24:50 +0300

    Implement TF-IDF vectorization for text data

--- ai/engine.py
+++ ai/engine.py
@@ -1,1 +1,17 @@
-soon
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer()
+
+my_list2 = []
+my_list1 = []
+
+with open("dataset/example.csv") as file:
+    read = file.readlines()
+    for line in read[1:]:
+        code, label = line.split(",")
+        label = label.strip()
+        my_list1.append(code)
+        my_list2.append(label)
+vectorizer.fit(my_list1)
+x = vectorizer.transform(my_list1)
+print(vectorizer.get_feature_names_out())
